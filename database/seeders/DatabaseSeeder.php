cae1573797dd9ce944c60471d4a107307b969736
OS <goloborodko@inbox.ru>
2021-06-08 08:37:53 +0300

    correct seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            TagSeeder::class,
             ArticleSeeder::class,
         ]);
     }
