03a1e4d82261378e0fc22d7ced4b8620fe4e050d
OS <goloborodko@inbox.ru>
2021-06-08 08:34:52 +0300

    correct seeders

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ArticleFactory extends Factory
@@ -26,8 +27,9 @@ class ArticleFactory extends Factory
             'title' => $this->faker->sentence(2, true),
             'annotation' => $this->faker->words(3, true),
             'body' => $this->faker->paragraph(20),
-            'published' => $this->faker->randomKey(['true', 'false']),
+            'published' => $this->faker->boolean(),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'user_id' => User::all()->random(),
 
         ];
     }
--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Article;
 use App\Models\Tag;
 use App\Models\User;
+use Database\Factories\TagFactory;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
@@ -18,13 +19,21 @@ class ArticleSeeder extends Seeder
     public function run()
     {
         Article::factory()
-            ->count(20)
-            ->state( new Sequence(
-                fn () => ['user_id' => User::all()->random()],
-            ))
+            ->count(5)
             ->create()
             ->each(function (Article $article) {
-                $article->tags()->saveMany(Tag::all()->random(2));
+                $article->tags()->saveMany(Tag::factory()->count(2)->make());
+            });
+
+        Tag::factory()
+            ->count(5)
+            ->create();
+
+        Article::factory()
+            ->count(15)
+            ->create()
+            ->each( function (Article $article) {
+               $article->tags()->saveMany(Tag::all()->random(2));
             });
     }
 }
