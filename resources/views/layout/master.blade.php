0667e1def545715ecb2817f38a26944161e1b259
OS <goloborodko@inbox.ru>
2021-07-22 11:58:12 +0300

    try test broadcasting

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
 
     <title>@yield('title', 'Статьи')</title>
